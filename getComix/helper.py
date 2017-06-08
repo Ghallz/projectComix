6a674eae686c781d0a2ff2d1c20ca79eadd46b52
Fillipe Galiza <fillipegaliza@hotmail.com>
2017-06-07 21:59:47 -0300

    get data from library page PARTIAL COMMIT

--- getComix/helper.py
+++ getComix/helper.py
@@ -2,12 +2,27 @@ import requests
 import json
 from django.conf import settings
 
-class Request(object):
-	"""docstring for Request"""
-	def __init__(self):
-		self.urlLibrary = settings.COMIX_URL['URL_LIBRARY']
-		self.urlComix = settings.COMIX_URL['URL_COMIX']
-		self.savePath = settings.COMIX_URL['SAVE_PATH']
 
-	def getComix():
-		
+class Request(object):
+    """docstring for Request"""
+
+    def __init__(self):
+        self.urlLibrary = settings.COMIX_URL['URL_LIBRARY']
+        self.urlComix = settings.COMIX_URL['URL_COMIX']
+        self.savePath = settings.COMIX_URL['SAVE_PATH']
+
+    def getTotalPages(self):
+        response = requests.get(settings.COMIX_URL['URL_LIBRARY'] + '1')
+        response = response.json()
+        totalPages = response['num']
+        return totalPages
+
+    def getAllComix(self):
+        totalPages = self.getTotalPages()
+        for i in range(totalPages):
+       		response = requests.get(settings.COMIX_URL['URL_LIBRARY'] + i)
+       		response = response.json()
+       		libraryPage = response['dados']
+       		
+
+        return teste
