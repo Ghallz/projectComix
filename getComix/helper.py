00344d5aae7687c4792c4b334e065e5882fcac81
Fillipe Galiza <fillipegaliza@hotmail.com>
2017-06-14 01:33:53 -0300

    almost finished

--- getComix/helper.py
+++ getComix/helper.py
@@ -1,8 +1,11 @@
-  import requests
+import requests
 import json
 from django.conf import settings
 from pprint import pprint
-from .models import Comix
+from .models import Comix, Chapter
+import re
+import os
+
 
 class Request(object):
     """docstring for Request"""
@@ -27,7 +30,7 @@ class Request(object):
             for j in libraryPage:
                 response = requests.get(
                     settings.COMIX_URL['URL_COMIX'] + str(j['id']))
-                comixJson = response.json()
+                comixJson = response.json()[0]
                 comix = Comix()
                 comix.plot = comixJson['sinopse']
                 comix.title = comixJson['titulo']
@@ -36,8 +39,33 @@ class Request(object):
                 comix.adult = comixJson['maioridade']
                 comix.views = comixJson['views']
                 comix.type = comixJson['tipo']
-                comix.chapters = comixJson['capitulos'].len()
+                comix.chapters = len(comixJson['capitulos'])
+                # comix.save()
 
-                return comix
+                for k in range(len(comixJson['capitulos'])):
+                    chapterJson = comixJson['capitulos'][k]
+                    comixChapter = Chapter()
+                    comixChapter.mainPage = chapterJson['arquivo']
+                    comixChapter.cover = chapterJson['url_capa']
+                    # comixChapter.save()
+
+                    mainChapterPage = requests.get(chapterJson['arquivo'])
+                    raw = mainChapterPage.text
+                    links = re.findall('(?<=data-lazy=")(.*?)(?=\")', raw)
+                    return links[4]
+
+                    for l in links:
+                        match = re.search('.png', links[k])
+                        if(match is not None):
+                            """save file """
+                            folder = os.path.abspath(os.path.join(
+                                os.sep, 'home', 'galiza', 'comixDatabase', comixJson['titulo'], chapterJson['capitulo']))
+                            fileName = l + ".jpg"
+                            """
+                            use os.path to check is folder exists
+                            https://stackoverflow.com/questions/273192/how-can-i-create-a-directory-if-it-does-not-exist
+                            """
+                            # request.urlretrieve(links[k], filename)
+                            return folder
             break
         # return teste
--- getComix/models.py
+++ getComix/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-import helper
 import uuid
 # Create your models here.
 
 
-class Comix(object):
+class Comix(models.Model):
     """docstring for Comix"""
     plot = models.TextField()
     title = models.CharField(max_length=300)
@@ -13,6 +12,12 @@ class Comix(object):
     views = models.BigIntegerField()
     cover = models.CharField(max_length=50)
 
-class Chapter(object):
+    def __str__(self):
+        return self.plot
+
+
+class Chapter(models.Model):
     """docstring for Chapter"""
-    
+    comixId = models.ForeignKey(Comix, on_delete=models.CASCADE)
+    mainPage = models.CharField(max_length=200)
+    cover = models.CharField(max_length=200)
