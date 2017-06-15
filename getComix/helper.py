d50db87e635928b378cf888279ae838360c1e3cd
Fillipe Galiza <fillipegaliza@hotmail.com>
2017-06-15 04:37:59 -0300

    tesesting how to save image

--- getComix/helper.py
+++ getComix/helper.py
@@ -3,6 +3,7 @@ import json
 from django.conf import settings
 from pprint import pprint
 from .models import Comix, Chapter
+import urllib
 import re
 import os
 
@@ -22,8 +23,15 @@ class Request(object):
         return totalPages
 
     def getAllComix(self):
+        url = 'http://hqultimate.com/leitor/hq/13 Artefato/01/01.jpg'
+        testfile = urllib.request.urlretrieve(url, "teste.jpg")
+        testfile.retrieve
+
+        return 'fileDownloaded'
+
         totalPages = self.getTotalPages()
-        for i in range(1, totalPages):
+        for i in range(1, 2):
+            # for i in range(1, totalPages):
             response = requests.get(settings.COMIX_URL['URL_LIBRARY'] + str(i))
             response = response.json()
             libraryPage = response['dados']
@@ -41,6 +49,14 @@ class Request(object):
                 comix.type = comixJson['tipo']
                 comix.chapters = len(comixJson['capitulos'])
                 # comix.save()
+                folder = os.path.abspath(os.path.join(
+                    os.sep, 'home', 'galiza', 'comixDatabase', comixJson['titulo']))
+
+                if not os.path.isdir(folder):
+                    print('\n\nCRIANDO PASTA\n\n' +
+                          str(comixJson['titulo']))
+                    os.mkdir(folder)
+                    os.chmod(folder, 0o777)
 
                 for k in range(len(comixJson['capitulos'])):
                     chapterJson = comixJson['capitulos'][k]
@@ -52,7 +68,7 @@ class Request(object):
                     mainChapterPage = requests.get(chapterJson['arquivo'])
                     raw = mainChapterPage.text
                     links = re.findall('(?<=data-lazy=")(.*?)(?=\")', raw)
-                    return links[4]
+                    return links
 
                     for l in links:
                         match = re.search('.png', links[k])
@@ -60,12 +76,25 @@ class Request(object):
                             """save file """
                             folder = os.path.abspath(os.path.join(
                                 os.sep, 'home', 'galiza', 'comixDatabase', comixJson['titulo'], chapterJson['capitulo']))
-                            fileName = l + ".jpg"
+
+                            if not os.path.isdir(folder):
+                                print('\n\nCRIANDO PASTA\n\n' +
+                                      str(comixJson['titulo']))
+                                os.mkdir(folder)
+                                os.chmod(folder, 0o777)
+
+                            fileName = str(l) + ".jpg"
+
+                            request.urlretrieve(links[k], folder + fileName)
                             """
-                            use os.path to check is folder exists
-                            https://stackoverflow.com/questions/273192/how-can-i-create-a-directory-if-it-does-not-exist
                             """
-                            # request.urlretrieve(links[k], filename)
-                            return folder
+                            user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+                            headers = { 'User-Agent' : user_agent }
+                            imgRequest = urllib.Request(
+                                imgUrl, headers=headers)
+                            imgData = urllib.urlopen(imgRequest).read()
+
+                            print("SALVANDO IMAGEM" + fileName)
+            print("Finalizando " + comixJson['titulo'])
             break
         # return teste
